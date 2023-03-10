f4f7b4a256248817114fdf679bd692a95863722b
subirdas29 <dasjoy104@gmail.com>
2023-03-11 02:18:31 +0600

    bg image added

--- src/Pages/Home/Deals of the week/AllDealsBook.js
+++ src/Pages/Home/Deals of the week/AllDealsBook.js
@@ -6,11 +6,18 @@ const AllDealsBook = ({dealBook}) => {
   <div className='my-5 ml-8 flex items-center gap-6'>
     <img src={img} className="w-24 h-36 rounded-lg shadow-2xl" />
     <div className='content-center '>
-      <h1 className="text-sm font-bold">{cat_name}</h1>
-      <h1 className="text-xl font-bold mb-5">{name}</h1>
-      <h1 className="text-lg font-bold">Original Price:{original_price}</h1>
-      <h1 className="text-lg font-bold">Reasale Price:{resale_price}</h1>
-   
+      <h1 className="text-sm font-bold text-[#999999]">{cat_name}</h1>
+      {/* <h1 className="text-xl font-bold mb-5">{name}</h1> */}
+      <div className='text-xl font-bold'>
+      {
+                        name.length > 25 ?
+                            <div >{name.slice(0, 25) + '...'} </div>
+                            :
+                            name
+                    }
+      </div>
+      <h1 className="text-base font-bold text-[#F44344] mt-5">Original Price: {original_price}</h1>
+      <h1 className="text-base font-bold text-[#F44344]">Resale Price: {resale_price}</h1>
    
   </div>
 
--- src/Pages/Home/Deals of the week/DealsWeek.js
+++ src/Pages/Home/Deals of the week/DealsWeek.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useLoaderData } from 'react-router-dom';
 import AllDealsBook from './AllDealsBook';
-
+import bg_banner_img from '../../../assets/Deals-book-bg/bh.png';
 const DealsWeek = () => {
     // const dealsBook = useLoaderData()
     // console.log(dealsBook)
@@ -15,12 +15,28 @@ const DealsWeek = () => {
         })
     },[])
     return (
-        <div>
+        <div className='bg-[#FBF1EB] '>
             <p className='text-4xl font-bold text-center  mb-10'>Best Deals Of The Week!</p>
             <div className='grid grid-cols-3 mx-10 '>
-            <div className="col-span-2 ">
-                <img src='https://cdn.shopify.com/s/files/1/0905/2012/files/bh01.jpg'></img>
+         
+          <div className="col-span-2 w-full relative"
+        //    style={{ backgroundImage: `url(${bg_banner_img})` }}
+           >
+                <img src={bg_banner_img}/>
+                <div className=" w-full  my-auto p-20 text-center absolute bottom-32 text-[#FFFFFF] grid justify-items-start ">
+           <h1 className="mb-5 text-xl text-left">NEW YEAR, NEW BOOKS
+</h1>
+           <h1 className="mb-5 font-bold text-5xl text-left">The 19 Best New 
+YA Books of January</h1>
+           <h1 className="mb-5 text-left">Without further ado, here are some of the most anticipated YA book
+coming out this January.</h1>
+          
+
+            <button className="bg-white px-14 mt-5 rounded-full text-black hover:text-white hover:outline outline-1  outline-white py-3 hover:bg-[#F44344] " >See More
+           
+            </button></div>
             </div>
+        
             <div className=''>
             {
                 dealsBook?.map(dealBook => <AllDealsBook key={dealBook.book_id} dealBook={dealBook}></AllDealsBook>)
