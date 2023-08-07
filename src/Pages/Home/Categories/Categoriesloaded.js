de6690dd95a92cbc453d5eb6ec8a0f7f9cbe5f3d
subirdas29 <dasjoy104@gmail.com>
2023-08-08 01:38:08 +0600

    category edited

--- src/Pages/Home/Categories/Categoriesloaded.js
+++ src/Pages/Home/Categories/Categoriesloaded.js
@@ -5,11 +5,11 @@ const Categoriesloaded = ({category}) => {
 
     const {cat_name,cat_img,category_id} = category
     return (
-        <div className="card  shadow-xl image-full my-10">
+        <div className="card  shadow-xl image-full">
         <figure><img src={cat_img} alt="Shoes" /></figure>
-        <div className="card-body text-center">
+        <div className="card-body justify-center text-center">
          <div>
-         <h2 className="text-white text-2xl font-bold">{cat_name}</h2>
+         <h2 className="text-white lg:text-3xl md:text-3xl text-4xl font-bold">{cat_name}</h2>
           
          </div>
           <div className="flex justify-center gap-2 my-5">     
