78014bbb66b11b29b0e12bf0db44def95687075f
subirdas29 <dasjoy104@gmail.com>
2023-08-09 22:15:07 +0600

    slider bar responsive

--- src/Pages/Home/ClientsSay/WhatClientSay.js
+++ src/Pages/Home/ClientsSay/WhatClientSay.js
@@ -15,7 +15,7 @@ const WhatClientSay = () => {
       <h1 className="text-4xl font-bold text-center mb-10">What Client Says</h1>
       <Carousel itemsToShow={1}>
         <Item>
-          <section className="my-8 dark:bg-white dark:text-black drop-shadow-xl">
+          <section className="my-8 mx-8 dark:bg-white dark:text-black drop-shadow-xl">
             <div className="container flex flex-col items-center p-4 mx-auto space-y-6 md:p-8">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
@@ -26,22 +26,22 @@ const WhatClientSay = () => {
                 <polygon points="328.375 384 332.073 458.999 256.211 406.28 179.924 459.049 183.625 384 151.586 384 146.064 496 182.756 496 256.169 445.22 329.242 496 365.936 496 360.414 384 328.375 384"></polygon>
                 <path d="M415.409,154.914l-2.194-48.054L372.7,80.933,346.768,40.414l-48.055-2.2L256,16.093,213.287,38.219l-48.055,2.2L139.3,80.933,98.785,106.86l-2.194,48.054L74.464,197.628l22.127,42.715,2.2,48.053L139.3,314.323l25.928,40.52,48.055,2.195L256,379.164l42.713-22.126,48.055-2.195,25.928-40.52L413.214,288.4l2.195-48.053,22.127-42.715Zm-31.646,76.949L382,270.377l-32.475,20.78-20.78,32.475-38.515,1.76L256,343.125l-34.234-17.733-38.515-1.76-20.78-32.475L130,270.377l-1.759-38.514L110.5,197.628,128.237,163.4,130,124.88,162.471,104.1l20.78-32.474,38.515-1.76L256,52.132l34.234,17.733,38.515,1.76,20.78,32.474L382,124.88l1.759,38.515L401.5,197.628Z"></path>
               </svg>
-              <p className="px-6 py-2 text-2xl font-semibold text-center sm:font-bold sm:text-3xl md:text-4xl lg:max-w-2xl xl:max-w-4xl dark:text-black">
+              <p className="px-6 py-2  font-semibold text-center sm:font-bold md:text-4xl sm:text-3xl text-lg lg:max-w-2xl xl:max-w-4xl dark:text-black">
                 "This is the best website for resale book.I got my favourite book from here with cheap rate. Thanks for the book "
               </p>
               <div className="flex justify-center space-x-3">
                 <img
                   src={img1}
                   alt=""
-                  className="w-20 h-20 bg-center bg-cover rounded-md dark:bg-gray-500 dark:bg-gray-700"
+                  className="sm:w-20 sm:h-20 w-14 h-14 bg-center bg-cover rounded-md dark:bg-gray-500 dark:bg-gray-700"
                 />
                 <div>
-                  <p className="leadi">Leroy Jenkins</p>
-                  <p className="text-sm leadi dark:text-gray-600">
+                  <p className="leadi sm:text-4xl text-lg ">Leroy Jenkins</p>
+                  <p className="sm:text-sm text-xs leadi dark:text-gray-600">
                   California
                   </p>
                   <a
-                    className="flex items-center py-2 space-x-1 text-sm dark:text-[#F44344]"
+                    className="flex items-center py-2 space-x-1 sm:text-sm text-xs dark:text-[#F44344]"
                     href="/"
                   >
                     <span>Read full story</span>
