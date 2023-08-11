cd2d4d763ef6c3331a9f1164cbac55df28e691ca
subirdas29 <dasjoy104@gmail.com>
2023-08-11 21:07:47 +0600

    scroll navbar

--- src/Pages/Shared/Navbar/Navbar.js
+++ src/Pages/Shared/Navbar/Navbar.js
@@ -1,12 +1,37 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { AuthContext } from '../../../Context/AuthProvider';
 import useAdmin from '../../../hooks/useAdmin';
 import useSeller from '../../../hooks/useSeller';
 
 const Navbar = () => {
+
+    const transparentBackgroundClass = 'navbar bg-transparent';
+    const solidBackgroundClass = 'navbar bg-[#FFFFFF] drop-shadow-lg';
+    const fixedNavbarClass = 'fixed top-0 left-0 w-full z-50';
+
+    const [isFixed, setIsFixed] = useState(false);
+    const [isTransparent, setIsTransparent] = useState(true);
+  
+    useEffect(() => {
+      const handleScroll = () => {
+        const shouldFixNavbar = window.scrollY > 0;
+        setIsFixed(shouldFixNavbar);
+  
+        const shouldUseTransparentBackground = window.scrollY === 0;
+        setIsTransparent(shouldUseTransparentBackground);
+      };
+  
+      window.addEventListener('scroll', handleScroll);
+      return () => {
+        window.removeEventListener('scroll', handleScroll);
+      };
+    }, []);
+
+    
     const {user,logOut} = useContext(AuthContext)
     console.log(user)
+
 
 
     const [isAdmin] = useAdmin(user?.email)
@@ -55,7 +80,8 @@ const Navbar = () => {
     }
 </React.Fragment>
     return (
-        <div className="navbar bg-[#FFFFFF] flex justify-between border-b border-slate-100">
+        <div className={`${isFixed ? fixedNavbarClass : ''} ${isTransparent ? transparentBackgroundClass : solidBackgroundClass}`}>
+        <div className="navbar bg-[#FFFFFF] flex justify-between">
         <div className="navbar-start">
             <div className="dropdown">
                 <label tabIndex={0} className="btn btn-ghost lg:hidden">
@@ -74,7 +100,7 @@ const Navbar = () => {
                 
             </ul>
         </div>
-     
+        </div>
     
     </div>
     );
