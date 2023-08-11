75bd77f7487166ef4e0d0743d11a4a403cc8a0d2
subirdas29 <dasjoy104@gmail.com>
2023-08-11 20:41:37 +0600

    deals margin change

--- src/Pages/Home/Deals of the week/DealsWeek.js
+++ src/Pages/Home/Deals of the week/DealsWeek.js
@@ -27,9 +27,9 @@ const DealsWeek = () => {
           //    style={{ backgroundImage: `url(${bg_banner_img})` }}
         >
           <img className="lg:min-h-[1020px] min-h-[600px] w-full lg:mb-0 mb-6 " src={bg_banner_img} />
-          <div className=" w-full  my-auto lg:p-20 md:p-14 sm:p-12 px-10 text-center absolute lg:bottom-44 md:bottom-40 sm:bottom-32 bottom-20  text-[#FFFFFF] grid justify-items-start ">
-            <h1 className="lg:mb-5 mb-2 lg:text-xl md:text-xl sm:text-sm text-xs text-left">NEW YEAR, NEW BOOKS</h1>
-            <h1 className="lg:mb-5 mb-2  font-bold lg:text-5xl md:text-5xl text-2xl text-left">
+          <div className=" w-full  my-auto lg:p-20 md:p-14 sm:p-12 px-10 text-center absolute lg:bottom-44 md:bottom-40 sm:bottom-32 bottom-28  text-[#FFFFFF] grid justify-items-start ">
+            <h1 className="lg:mb-5 mb-2 lg:text-xl md:text-xl sm:text-lg text-xs text-left">NEW YEAR, NEW BOOKS</h1>
+            <h1 className="lg:mb-5 mb-2  font-bold lg:text-5xl md:text-5xl sm:text-4xl text-2xl text-left">
               The 19 Best New YA Books of January
             </h1>
             <h1 className="lg:mb-5 text-left">
@@ -38,7 +38,7 @@ const DealsWeek = () => {
              
             </h1>
 
-            <button className="bg-white lg:px-14 md:px-14 sm:px-8 px-4 lg:mb-5 md:mt-5 mt-5 rounded-full text-black hover:text-white hover:outline outline-1  outline-white lg:py-3 md:py-3 sm:py-2 py-1 lg:text-lg  text-sm hover:bg-[#F44344] ">
+            <button className="bg-white lg:px-14 md:px-14 sm:px-10 px-4 lg:mb-5 md:mt-5 mt-5 rounded-full text-black hover:text-white hover:outline outline-1  outline-white lg:py-3 md:py-3 sm:py-3 py-1 lg:text-lg  text-sm hover:bg-[#F44344] ">
               See More
             </button>
           </div>
